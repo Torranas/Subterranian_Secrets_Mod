cf10ec28ea8b35c18c9236a83623d288dc5c0c66
Torranas <Torranas2003@gmail.com>
2023-03-24 05:51:36 +0000

    Added Ores for the malachite mineral

--- src/main/java/net/wraith/s_s/SubterraneanSecrets.java
+++ src/main/java/net/wraith/s_s/SubterraneanSecrets.java
@@ -11,6 +11,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.wraith.block.ModBlocks;
 import net.wraith.s_s.item.ModCreativeModeTabs;
 import net.wraith.s_s.item.ModItems;
 import org.slf4j.Logger;
@@ -18,15 +19,14 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(SubterraneanSecrets.MOD_ID)
 public class SubterraneanSecrets {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "s_s";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public SubterraneanSecrets() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -46,6 +46,21 @@ public class SubterraneanSecrets {
         if(event.getTab() == ModCreativeModeTabs.SUBTERRANEAN_SECRETS_TAB){
             event.accept(ModItems.MALACHITE);
             event.accept(ModItems.RAW_MALACHITE);
+            event.accept(ModBlocks.MALACHITE_BLOCK);
+            event.accept(ModBlocks.MALACHITE_ORE);
+            event.accept(ModBlocks.DEEPSLATE_MALACHITE_ORE);
+            event.accept(ModBlocks.NETHERRACK_MALACHITE_ORE);
+            event.accept(ModBlocks.ENDSTONE_MALACHITE_ORE);
+        }
+        if(event.getTab() == CreativeModeTabs.BUILDING_BLOCKS){
+            event.accept(ModBlocks.MALACHITE_BLOCK);
+
+        }
+        if (event.getTab() == CreativeModeTabs.NATURAL_BLOCKS){
+            event.accept(ModBlocks.MALACHITE_ORE);
+            event.accept(ModBlocks.DEEPSLATE_MALACHITE_ORE);
+            event.accept(ModBlocks.NETHERRACK_MALACHITE_ORE);
+            event.accept(ModBlocks.ENDSTONE_MALACHITE_ORE);
         }
     }
 
