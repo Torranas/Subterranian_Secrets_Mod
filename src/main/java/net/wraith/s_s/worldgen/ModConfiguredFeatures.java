54727feb62f4e327ada5c297e30c474b56e88f64
Torranas <Torranas2003@gmail.com>
2023-03-24 13:46:42 +0000

    Added malachite ore generation to all 3 dimensions

--- src/main/java/net/wraith/s_s/worldgen/ModConfiguredFeatures.java
+++ src/main/java/net/wraith/s_s/worldgen/ModConfiguredFeatures.java
@@ -4,22 +4,43 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
 import net.wraith.s_s.SubterraneanSecrets;
 import net.wraith.s_s.block.ModBlocks;
+
+import java.util.List;
 
 public class ModConfiguredFeatures {
     public static final ResourceKey<ConfiguredFeature<?, ?>> EBONY_KEY = registerKey("ebony");
 
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_MALACHITE_ORE_KEY = registerKey("malachite_ore");
+    public static final ResourceKey<ConfiguredFeature<?, ?>> END_MALACHITE_ORE_KEY = registerKey("end_malachite_ore");
+    public static final ResourceKey<ConfiguredFeature<?, ?>> NETHER_MALACHITE_ORE_KEY = registerKey("nether_malachite_ore");
+
+
     public static void bootstrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
+        RuleTest stonereplaceables = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslatereplaceables = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+        RuleTest netherrackreplaceables = new BlockMatchTest(Blocks.NETHERRACK);
+        RuleTest endstonereplaceables = new BlockMatchTest(Blocks.END_STONE);
+
+        List<OreConfiguration.TargetBlockState> overworldMalachiteOres = List.of(OreConfiguration.target(stonereplaceables,
+                ModBlocks.MALACHITE_ORE.get().defaultBlockState()),
+                OreConfiguration.target(deepslatereplaceables, ModBlocks.DEEPSLATE_MALACHITE_ORE.get().defaultBlockState()));
 
         register(context, EBONY_KEY, Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
                 BlockStateProvider.simple(ModBlocks.EBONY_LOG.get()),
@@ -27,6 +48,12 @@ public class ModConfiguredFeatures {
                 BlockStateProvider.simple(ModBlocks.EBONY_LEAVES.get()),
                 new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 4),
                 new TwoLayersFeatureSize(1, 0, 2)).build());
+
+        register(context, OVERWORLD_MALACHITE_ORE_KEY, Feature.ORE, new OreConfiguration(overworldMalachiteOres, 10));
+        register(context, END_MALACHITE_ORE_KEY, Feature.ORE, new OreConfiguration(endstonereplaceables,
+                ModBlocks.ENDSTONE_MALACHITE_ORE.get().defaultBlockState(), 10));
+        register(context, NETHER_MALACHITE_ORE_KEY, Feature.ORE, new OreConfiguration(netherrackreplaceables,
+                ModBlocks.NETHERRACK_MALACHITE_ORE.get().defaultBlockState(), 10));
     }
 
 
--- src/main/java/net/wraith/s_s/worldgen/ModPlacedFeatures.java
+++ src/main/java/net/wraith/s_s/worldgen/ModPlacedFeatures.java
@@ -8,7 +8,9 @@ import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 import net.wraith.s_s.SubterraneanSecrets;
@@ -19,11 +21,26 @@ import java.util.List;
 public class ModPlacedFeatures {
     public static final ResourceKey<PlacedFeature> EBONY_PLACED_KEY = createKey("ebony_placed");
 
+    public static final ResourceKey<PlacedFeature> MALACHITE_PLACED_KEY = createKey("malachite_placed");
+    public static final ResourceKey<PlacedFeature> END_MALACHITE_PLACED_KEY = createKey("end_malachite_placed");
+    public static final ResourceKey<PlacedFeature> NETHER_MALACHITE_PLACED_KEY = createKey("nether_malachite_placed");
+
+
     public static void bootstrap(BootstapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
         register(context, EBONY_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.EBONY_KEY),
                 VegetationPlacements.treePlacement(PlacementUtils.countExtra(3, 0.1f, 2), ModBlocks.EBONY_SAPLING.get()));
+
+        register(context, MALACHITE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.OVERWORLD_MALACHITE_ORE_KEY),
+                ModOrePlacement.commonOrePlacement(16, // veins per chunk
+                        HeightRangePlacement.uniform(VerticalAnchor.absolute(-80), VerticalAnchor.absolute(80))));
+        register(context, END_MALACHITE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.END_MALACHITE_ORE_KEY),
+                ModOrePlacement.commonOrePlacement(16, // veins per chunk
+                        HeightRangePlacement.uniform(VerticalAnchor.absolute(-80), VerticalAnchor.absolute(80))));
+        register(context, NETHER_MALACHITE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.NETHER_MALACHITE_ORE_KEY),
+                ModOrePlacement.commonOrePlacement(16, // veins per chunk
+                        HeightRangePlacement.uniform(VerticalAnchor.absolute(-80), VerticalAnchor.absolute(80))));
     }
 
 
